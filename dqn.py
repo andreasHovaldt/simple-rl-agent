515c75a31622c1f52e8aedd31847639f5691871a
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-08-10 17:49:58 +0200

    Added DQN agent

---
+++ dqn.py
@@ -0,0 +1,36 @@
+import random
+from collections import namedtuple, deque
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+
+class ReplayMemory(object):
+    def __init__(self, size) -> None:
+        self.memory = deque([], maxlen=size)
+    
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions) -> None:
+        super().__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+    
+    def forward(self, x) -> torch.Tensor: # (batch_size, n_observations)
+        x = F.relu(self.layer1(x)) # (batch_size, 128)
+        x = F.relu(self.layer2(x)) # (batch_size, 128)
+        return self.layer3(x) # (batch_size, n_actions)
